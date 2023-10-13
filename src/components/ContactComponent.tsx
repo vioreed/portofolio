4738ced0a192ddd8ffaa15f6dc1ddfc64626c3b9
Vioreed <predimolanaz23@gmail.com>
2023-10-13 15:15:14 +0700

    feat: disable button send message

--- src/components/ContactComponent.tsx
+++ src/components/ContactComponent.tsx
@@ -48,7 +48,7 @@ const ContactComponent: React.FC<ContactComponentProps> = ({ isDarkMode }) => {
                                         <div className="flex items-center justify-end pb-4">
 
                                             <button className={`flex items-center gap-x-1 px-4 py-2 border border-${isDarkMode ? "[rgba(255,255,255,.15)] text-[#F8F8FB] hover:text-[#64FFDA] hover:border-[#64FFDA]" : "[#ced4da] text-gray-600 hover:text-[#0F162B] hover:border-[#0F162B]"} `}
-                                                style={{ borderRadius: "0.25rem" }}>
+                                                style={{ borderRadius: "0.25rem" }} disabled>
                                                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-5 h-5">
                                                     <path strokeLinecap="round" strokeLinejoin="round" d="M6 12L3.269 3.126A59.768 59.768 0 0121.485 12 59.77 59.77 0 013.27 20.876L5.999 12zm0 0h7.5" />
                                                 </svg>
