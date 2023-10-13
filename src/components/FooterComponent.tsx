bc23857369c71a42cf91089559ff42efde983881
Vioreed <predimolanaz23@gmail.com>
2023-10-13 19:41:04 +0700

    feat: footer component responsive design

--- src/components/FooterComponent.tsx
+++ src/components/FooterComponent.tsx
@@ -12,8 +12,8 @@ const FooterComponent: React.FC<FooterComponentProps> = ({
     return (
         <div className={`flex font-inter items-center h-16 bottom-0 left-0 w-full shadow-md ${isDarkMode ? "bg-[#0F162B] text-[#F8F8FB] border-b border-[rgba(255,255,255,.15)]" : "bg-[#F8F8FB] text-[#0F162B]"
             }`}>
-            <div className="flex items-center justify-between w-full mx-96">
-                <a href="/" className={`justify-start text-base font-semibold cursor-pointer ${isDarkMode ? "text-[#F8F8FB] hover:text-[#64FFDA]" : "text-[#0F162B]"}`}>
+            <div className="flex items-center justify-center text-center md:justify-between w-full mx-4 lg:mx-96">
+                <a href="/" className={`hidden md:block justify-start text-base font-semibold cursor-pointer ${isDarkMode ? "text-[#F8F8FB] hover:text-[#64FFDA]" : "text-[#0F162B]"}`}>
                     <p>Vioreed</p>
                 </a>
                 <p className="text-sm items-center">
